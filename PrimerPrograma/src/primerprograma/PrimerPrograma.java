72884479eb200e0011587407236b1c3ae44cb8d9
davidblancolopez <daviddbl94@gmail.com>
2016-09-15 19:31:34 +0200

    Update PrimerPrograma.java

--- PrimerPrograma/src/primerprograma/PrimerPrograma.java
+++ PrimerPrograma/src/primerprograma/PrimerPrograma.java
@@ -7,6 +7,11 @@ public class PrimerPrograma {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
     }
     
 }
